1bd54cad5b7154e65c2089ce2e5869f8cf7d6aaa
Mickae Truman <mtruman@nyit.edu>
2019-03-25 02:44:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   
 attr_accessor :sender, :receiver, :status 
 
-def initialize(transfer_amount)
+def initialize(sender, receiver, transfer_amount)
   @sender = sender
   @receiver = receiver
   @status = 'pending'
